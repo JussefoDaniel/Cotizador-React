003da0d4f7804abe0862bf84e6e70e91a3c2abdb
Jussefo91 <jussefspira@users.noreply.github.com>
2020-06-24 00:29:25 -0500

    Update Formulario.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,54 +1,81 @@
-import React from 'react';
+import React from "react";
+import styled from "@emotion/styled";
+
+const Campo = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+
+const Boton = styled.button`
+    background-color: #00838F;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+    &:hover {
+        background-color: #26C6DA;
+        cursor: pointer;
+    }
+`;
 
 const Formulario = () => {
-    return ( 
-        <form>
-            <div>
-                <label>Marca</label>
-                <select>
-                    <option value="">-- Seleccione --</option>
-                    <option value="americano">Americano</option>
-                    <option value="europeo">Europeo</option>
-                    <option value="asiatico">Asiatico</option>
-                </select>
-            </div>
+  return (
+    <form>
+      <Campo>
+        <Label>Marca</Label>
+        <Select>
+          <option value="">-- Seleccione --</option>
+          <option value="americano">Americano</option>
+          <option value="europeo">Europeo</option>
+          <option value="asiatico">Asiatico</option>
+        </Select>
+      </Campo>
 
-            <div>
-                <label>Año</label>
-                <select>
-                    <option value="">-- Seleccione --</option>
-                    <option value="2021">2021</option>
-                    <option value="2020">2020</option>
-                    <option value="2019">2019</option>
-                    <option value="2018">2018</option>
-                    <option value="2017">2017</option>
-                    <option value="2016">2016</option>
-                    <option value="2015">2015</option>
-                    <option value="2014">2014</option>
-                    <option value="2013">2013</option>
-                    <option value="2012">2012</option>
-                </select>
-            </div>
+      <Campo>
+        <Label>Año</Label>
+        <Select>
+          <option value="">-- Seleccione --</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+          <option value="2016">2016</option>
+          <option value="2015">2015</option>
+          <option value="2014">2014</option>
+          <option value="2013">2013</option>
+          <option value="2012">2012</option>
+        </Select>
+      </Campo>
 
-            <div>
-                <label>Plan</label>
-                <input 
-                    type="radio"
-                    name="plan"
-                    value="basico"
-                /> Básico
+      <Campo>
+        <Label>Plan</Label>
+        <InputRadio type="radio" name="plan" value="basico" /> Básico
+        <InputRadio type="radio" name="plan" value="completo" /> Completo
+      </Campo>
 
-                <input 
-                    type="radio"
-                    name="plan"
-                    value="completo"
-                /> Completo
-            </div>
-
-            <button type="button">Cotizar</button>
-        </form>
-
-     );
-}
+      <Boton type="button">Cotizar</Boton>
+    </form>
+  );
+};
 
 export default Formulario;
